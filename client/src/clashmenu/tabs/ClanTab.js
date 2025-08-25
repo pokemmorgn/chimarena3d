936a1364f950530740bf85bb9be72c64d841bd9b
pokemmorgn <pokemmorgn@gmail.com>
2025-08-25 22:18:20 +0200

    Update ClanTab.js

--- client/src/clashmenu/tabs/ClanTab.js
+++ client/src/clashmenu/tabs/ClanTab.js
@@ -1,3 +1,8 @@
+/**
+ * ClanTab.js - Conteneur principal
+ * Décide quel état afficher (loading / no clan / clan content)
+ */
+
 import ClanCreateOverlay from './ClanCreateOverlay.js';
 import ClanAPI from '../../services/ClanAPI.js';
 import ClanContent from './clan/ClanContent.js';
@@ -7,8 +12,10 @@ class ClanTab {
     this.state = 'loading';
     this.currentUser = null;
     this.currentClan = null;
+
     this.container = null;
     this.tabElement = null;
+
     this.createOverlay = null;
     this.clanContent = null;
   }
@@ -17,10 +24,12 @@ class ClanTab {
     this.container = container;
     this.currentUser = currentUser;
 
+    // Crée le wrapper principal
     this.tabElement = document.createElement('div');
     this.tabElement.className = 'clan-tab';
     this.container.appendChild(this.tabElement);
 
+    // Overlay création de clan
     this.createOverlay = new ClanCreateOverlay();
     this.createOverlay.initialize(this.container);
     this.createOverlay.on('clan:created', (clanData) => {
@@ -28,16 +37,22 @@ class ClanTab {
       this.setState('has_clan');
     });
 
+    // Vérifie état du clan
     await this.checkClanStatus();
   }
 
   async checkClanStatus() {
     this.setState('loading');
-    const res = await ClanAPI.getMyClan();
-    if (res.success && res.data) {
-      this.currentClan = res.data;
-      this.setState('has_clan');
-    } else {
+    try {
+      const res = await ClanAPI.getMyClan();
+      if (res.success && res.data) {
+        this.currentClan = res.data;
+        this.setState('has_clan');
+      } else {
+        this.setState('no_clan');
+      }
+    } catch (err) {
+      console.error('❌ Clan check failed:', err);
       this.setState('no_clan');
     }
   }
@@ -49,8 +64,14 @@ class ClanTab {
 
   render() {
     if (this.state === 'loading') {
-      this.tabElement.innerHTML = `<div class="clan-loading"><div class="loading-spinner"></div><h3>Loading...</h3></div>`;
-    } else if (this.state === 'no_clan') {
+      this.tabElement.innerHTML = `
+        <div class="clan-loading">
+          <div class="loading-spinner"></div>
+          <h3>Loading...</h3>
+        </div>`;
+    }
+
+    else if (this.state === 'no_clan') {
       this.tabElement.innerHTML = `
         <div class="clan-no-clan">
           <h2>No Clan</h2>
@@ -58,11 +79,25 @@ class ClanTab {
         </div>`;
       this.tabElement.querySelector('#btn-create-clan')
         .addEventListener('click', () => this.createOverlay.open());
-    } else if (this.state === 'has_clan') {
-      // déléguer à ClanContent
+    }
+
+    else if (this.state === 'has_clan') {
       this.clanContent = new ClanContent(this.tabElement, this.currentUser, this.currentClan);
       this.clanContent.render();
     }
+  }
+
+  show() {
+    if (this.tabElement) this.tabElement.classList.add('active');
+    if (this.state === 'has_clan' && this.clanContent) {
+      // reconnecter au besoin
+      this.clanContent.render();
+    }
+  }
+
+  hide() {
+    if (this.tabElement) this.tabElement.classList.remove('active');
+    if (this.clanContent) this.clanContent.destroy();
   }
 }
 
